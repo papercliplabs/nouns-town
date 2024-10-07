95b0c6be7476cf5dcc68deb2e65e27141ddb96b3
spennyp <hello@spencerperkins.me>
2024-10-07 00:41:11 -0700

    web: minor copy change

--- web/src/components/TimeLeft.tsx
+++ web/src/components/TimeLeft.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { SECONDS_PER_DAY, SECONDS_PER_HOUR } from "@/utils/constants";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 interface TimeLeftProps {
@@ -7,13 +8,13 @@ interface TimeLeftProps {
 }
 
 export default function TimeLeft({ endTimeS }: TimeLeftProps) {
-  const [timeLeftFormatted, setTimeLeftFormatted] = useState<string>(" ");
+  const [timeLeftFormatted, setTimeLeftFormatted] = useState<string | undefined>(" ");
 
   useEffect(() => {
     const interval = setInterval(() => {
       const timeLeft = endTimeS - Math.floor(Date.now() / 1000);
       if (timeLeft <= 0) {
-        setTimeLeftFormatted("It's go time!");
+        setTimeLeftFormatted(undefined);
       } else {
         const days = Math.floor(timeLeft / SECONDS_PER_DAY);
         const hours = Math.floor((timeLeft % SECONDS_PER_DAY) / SECONDS_PER_HOUR);
@@ -27,5 +28,24 @@ export default function TimeLeft({ endTimeS }: TimeLeftProps) {
     return () => clearInterval(interval);
   });
 
-  return <div className="self-center whitespace-pre font-bold">{timeLeftFormatted}</div>;
+  return (
+    <div className="self-center whitespace-pre-wrap text-center font-bold">
+      {timeLeftFormatted != undefined ? (
+        timeLeftFormatted
+      ) : (
+        <>
+          If you already minted a ticket, claim your pass{" "}
+          <Link
+            href="https://nounstown.deform.cc/claim2024pass/"
+            target="_blank"
+            className="underline transition-all hover:brightness-75"
+            rel="noopener"
+          >
+            here
+          </Link>
+          !
+        </>
+      )}
+    </div>
+  );
 }
